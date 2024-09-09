66a9dc1af6b4a8c3ec872668f1615728ce839660
ankit-sysquo <ankit@sysquoinnovation.in>
2024-09-09 17:23:50 +0530

    Initial commit

---
+++ app/item.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import React from 'react'
+import { Posts } from '@prisma/client'
+import { useRouter, redirect } from 'next/navigation'
+
+interface Props {
+    posts: Posts
+}
+
+export default function items({ post }: Props){
+
+	const router = useRouter()
+
+    const handleDelete = async (id: number) => {
+        await fetch('/api/post?id=' + id, {
+            method: 'DELETE'
+        })
+
+        router.refresh()
+    }
+
+    return (
+        <div className='border-2 border-black p-3 rounded-md'>
+            <h2 className='mb-2'>ID: {post.id}</h2>
+            <h1 className='text-xl font-semibold'>{post.title}</h1>
+            <p>{post.content}</p>
+            <p>{post.tags}</p>
+
+            <div className='flex justify-end gap-3 mt-4 text-sm'>
+                <button className='font-semibold' onClick={() => router.push(`/update/${post.id}`)}>Update</button>
+                <button className='font-semibold text-red-500' onClick={() => handleDelete(post.id)}>Delete</button>
+            </div>
+        </div>
+    )
+
+
+}
---
+++ app/update/[id]/page.tsx
@@ -0,0 +1,62 @@
+"use client"
+import {useRouter, redirect} from 'next/navigation'
+import React, {useEffect, useState} from 'react'
+
+export default function updatePost({params}:{ params: {id : string} }){
+	const id = params.id;
+	const [title, setTitle] = useState('')
+	const [content, setContent] = useState('')
+	const [tags, setTags] = useState('')
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+    const router = useRouter()
+
+    const handleSubmit = async(e : any) => {
+    	e.preventDefault()
+        setIsLoading(true)
+
+        await fetch('/api/post', {
+            method: 'PUT', // Method put is to update
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title, content, tags, id
+            })
+        }).then((res) => {
+            console.log(res)
+        }).catch((e) => {
+            console.log(e)
+        })
+
+        setIsLoading(false)
+		router.push('/')
+    }
+
+    useEffect(() => {
+    	getData()
+    },[])
+
+    const getData = async () => {
+    	const res = await fetch('/api/post/' + id)
+        const json = await res.json()
+
+        if (!json) {
+            router.push('/404')
+            return
+        }
+
+        setTitle(json.post.title)
+        setContent(json.post.content)
+        setTags(json.post.tags)
+    }
+    
+    return (
+        <form className='w-[500px] mx-auto pt-20 flex flex-col gap-3' onSubmit={handleSubmit}>
+        	<div className='mx-auto pb-5 font-bold'>Update Post</div>
+            <input type="text" placeholder='Input your title' value={title} onChange={(e) => setTitle(e.target.value)} className='w-full border p-2 rounded-md' />
+            <textarea rows={10} placeholder='Input your content' value={content} onChange={(e) => setContent(e.target.value)} className='w-full border p-2 rounded-md' />
+            <input type="text" placeholder='Input your tags' value={tags} onChange={(e) => setTags(e.target.value)} className='w-full border p-2 rounded-md' />
+            <button className='button' disabled={isLoading}>{isLoading ? 'Loading ...' : 'Update'}</button>
+        </form>
+    )
+}
