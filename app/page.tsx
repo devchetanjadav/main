3968d51325fb241039e5e4dc29464a6b7d866b12
ankit-sysquo <ankit@sysquoinnovation.in>
2024-09-11 12:13:32 +0530

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
 import Link from 'next/link'
 import React from 'react'
-//import Item from './item'
+import Item from './item'
 
-/*const getPosts  = async () => {    
+const getPosts  = async () => {    
   // Because this is server components, we have to define the URL with http
   const res = await fetch(process.env.BASE_URL + '/api/post', { next: { revalidate: 0 } })
-  //console.log(res)
+  
   // Define the output to json, because if only res, it will return by any type
-  if(res){
-    const json = await res.json()
-     console.log(json)
-    return json
-  }else{
-    return
-  }
-}*/
+  const json = await res.json() 
+  return json
+}
 
 export default async function Notes(){
 
- // const posts = await getPosts()
+  const posts = await getPosts()
 
   return (
         <div className='w-[1200px] mx-auto py-10'>
@@ -27,9 +22,9 @@ export default async function Notes(){
             <Link href={"/create"} className='px-3 py-2 bg-zinc-900 hover:bg-zinc-800 rounded-md text-white'>Create</Link>
 
             <div className='grid grid-cols-3 gap-5 mt-8'>
-              {/*  {posts?.posts?.map((post: any, i: number) => (
+                {posts?.posts?.map((post: any, i: number) => (
                     <Item key={i} post={post} />
-                )).sort().reverse()}*/}
+                )).sort().reverse()}
             </div>
 
         </div>
